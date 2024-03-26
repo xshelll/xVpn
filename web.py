a0d414c55af53fcc77c48fda7c8224849d982d21
Reza Asadi <88263031+xshelll@users.noreply.github.com>
2024-03-26 14:37:11 +0330

    Update web.py

--- web.py
+++ web.py
@@ -15,29 +15,10 @@ def readFile():
                     configs += obj+'\n'
         return configs
 
-def encrypt(text):
-    BLOCK_SIZE = 16
-    def pad(data):
-        return data + (BLOCK_SIZE - len(data) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(data) % BLOCK_SIZE)
-    
-    def encrypt(message, key):
-        iv = os.urandom(BLOCK_SIZE)
-        aes = AES.new(key, AES.MODE_CBC, iv)
-        padded_message = pad(message)
-        encrypted_message = aes.encrypt(padded_message.encode('utf-8'))
-        return base64.b64encode(iv + encrypted_message).decode('utf-8')
-
-    message = text
-    key = b'xshell@@38461568'
-
-    encrypted_message = encrypt(message, key)
-    return encrypted_message
-
 @app.get("/getConfigs")
 def getConfigs():
     #if user_agent == 'xVpn':
     configs = readFile()
-    #encrypted = encrypt(configs)
     return configs
 
 app.run(host='0.0.0.0', port=55555)
